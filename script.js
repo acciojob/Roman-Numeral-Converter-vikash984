80ac40b0e59d42c19fdd3c945483d81bf1ec8a32
vikash984 <vikashk15072001@gmail.com>
2025-06-12 11:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function convertToRoman(num) {
     };
 
   //your code here
+ let result = '';
+
+  for (const [symbol, value] of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
+}
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
